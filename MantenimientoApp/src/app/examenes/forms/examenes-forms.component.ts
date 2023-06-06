ab3e70cd0d00b70de9049021fb0117e17b950d74
jessicaaviles8835 <jessica.aviles8835@est.uca.edu.ni>
2023-06-06 11:26:09 -0600

    Examenes terminado

--- MantenimientoApp/src/app/examenes/forms/examenes-forms.component.ts
+++ MantenimientoApp/src/app/examenes/forms/examenes-forms.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { Examenes } from "../interface/examenes";
+import { Examenes,  } from "../interface/examenes";
 import { ExamenesService } from "../services/examenes.service";
+import { AreaLab, Metodologia, Categoria, TipoMuestra, Unidad, Resultado  } from "../interface/examenes";
+import { Muestra } from "src/app/muestra/interface/muestra";
 
 @Component({
     selector: 'examenes-form',
@@ -13,6 +15,134 @@ import { ExamenesService } from "../services/examenes.service";
 export class ExamenesFormComponent{
     @Input()
     public examenesRecibida?: Examenes;
+
+    public areas: AreaLab[] = [
+        {
+            idAreaLabServicio: 1,
+           
+            descripcion: "Bacteriología",
+            estado: 1
+        },
+        {
+            idAreaLabServicio: 2,
+           
+            descripcion: "Inmunología",
+            estado: 1
+        },
+        {
+            idAreaLabServicio: 3,
+           
+            descripcion: "Coproparasitología",
+            estado: 1
+        },
+    ];
+    
+    public metodologias: Metodologia[] = [
+        {
+            idMetodologia: 1,
+           
+            descripcion: "Inmunoensayos",
+            estado: 1
+        },
+        {
+            idMetodologia: 2,
+           
+            descripcion: "Diagnóstico molecular",
+            estado: 1
+        },
+    ];
+
+    public categorias: Categoria[] = [
+        {
+            idCategoriaExamenes: 1,
+           
+            descripcion: "Análisis de fluidos corporales",
+            estado: 1
+        },
+        {
+            idCategoriaExamenes: 2,
+           
+            descripcion: "Endoscopia",
+            estado: 1
+        },
+        {
+            idCategoriaExamenes: 3,
+           
+            descripcion: "Biopsia",
+            estado: 1
+        },
+    ];
+
+    public muestras: TipoMuestra[] = [
+        {
+            idTipoMuestra: 1,
+           
+            descripcion: "Sangre",
+            estado: 1
+        },
+        {
+            idTipoMuestra: 2,
+           
+            descripcion: "Orina",
+            estado: 1
+        },
+        {
+            idTipoMuestra: 3,
+           
+            descripcion: "Suero",
+            estado: 1
+        },
+        {
+            idTipoMuestra: 4,
+           
+            descripcion: "Plasma",
+            estado: 1
+        },
+    ];
+
+    public medidas: Unidad[] = [
+        {
+            idUnidadMedidas: 1,
+           
+            descripcion: "Fentolitros",
+            estado: 1
+        },
+        {
+            idUnidadMedidas: 2,
+           
+            descripcion: "Gramos",
+            estado: 1
+        },
+        {
+            idUnidadMedidas: 3,
+           
+            descripcion: "Microgramos",
+            estado: 1
+        },
+        {
+            idUnidadMedidas: 4,
+           
+            descripcion: "Gramos por litro",
+            estado: 1
+        },
+    ];
+
+    public resultados: Resultado[] = [
+        {
+            idTipoResultado: 1,
+           
+            descripcion: "Positivo",
+            estado: 1
+        },
+        {
+            idTipoResultado: 2,
+           
+            descripcion: "Negativo",
+            estado: 1
+        },
+    ];
+
+
     public id?: number ;
     public idAreaLabServicio?: number;
     public idMetodologia?: number;
--- MantenimientoApp/src/app/examenes/interface/examenes.ts
+++ MantenimientoApp/src/app/examenes/interface/examenes.ts
@@ -16,3 +16,39 @@ export interface Examenes{
 
 
 }
+
+export interface AreaLab{
+    idAreaLabServicio?: number;
+    descripcion?: string;
+    estado?: number;
+}
+
+export interface Metodologia{
+    idMetodologia?: number;
+    descripcion?: string;
+    estado?: number;
+}
+
+export interface Categoria{
+    idCategoriaExamenes?: number;
+    descripcion?: string;
+    estado?: number;
+}
+
+export interface TipoMuestra{
+    idTipoMuestra?: number;
+    descripcion?: string;
+    estado?: number;
+}
+
+export interface Unidad{
+    idUnidadMedidas?: number;
+    descripcion?: string;
+    estado?: number;
+}
+
+export interface Resultado{
+    idTipoResultado?: number;
+    descripcion?: string;
+    estado?: number;
+}

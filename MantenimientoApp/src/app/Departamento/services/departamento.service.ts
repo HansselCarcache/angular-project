80045a2e689c91f023df41aee4368bb3f3727937
jessicaaviles8835 <jessica.aviles8835@est.uca.edu.ni>
2023-06-06 08:58:02 -0600

    borrando conflicto

--- MantenimientoApp/src/app/Departamento/services/departamento.service.ts
+++ MantenimientoApp/src/app/Departamento/services/departamento.service.ts
@@ -9,38 +9,25 @@ import { Observable, catchError, forkJoin, map, of, pipe } from "rxjs";
 })
 export class DepartamentoService {
 
-<<<<<<< HEAD
   private apiUrlDepartamentos:string = 'http://localhost:8081';
-=======
-  private apiUrlDepartamentos:string = 'http://localhost:8082';
->>>>>>> e966cc7a2105aabc974150b86f77ca46cfd25cdf
+
 
   constructor(private http: HttpClient) { }
 
   private getDepartamentosquest(url: string): Observable<Departamento[]>{
-<<<<<<< HEAD
+
       
  return this.http.get<Departamento[]>(url)
-=======
-      return this.http.get<Departamento[]>(url)
->>>>>>> e966cc7a2105aabc974150b86f77ca46cfd25cdf
           .pipe(
             catchError( () => of ([]))
           );
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e966cc7a2105aabc974150b86f77ca46cfd25cdf
   searchDepartamento(term: string): Observable<Departamento[]>{
       const url = `${this.apiUrlDepartamentos}/api/departamentos`;
       return this.getDepartamentosquest(url);
   }
 
-<<<<<<< HEAD
-
-=======
   // getPaisesById(idPais: number[]): Observable<(Pais | null)[]> {
   //   const url = `${this.apiUrlPais}/api/pais`;
   //   const requests: Observable<Pais | null>[] = [];
@@ -53,5 +40,5 @@ export class DepartamentoService {
 
   //   return forkJoin(requests);
   // }
->>>>>>> e966cc7a2105aabc974150b86f77ca46cfd25cdf
+
 }
